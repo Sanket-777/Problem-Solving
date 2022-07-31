a335c728b5e18cdef2ad73ee2cf974be93cc47b1
Sanket kumbhar <ksanket114402@gmail.com>
2022-07-31 22:29:21 +0530

    Time: 2 ms (46.04%), Space: 6.5 MB (71.25%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -6,7 +6,7 @@ public:
         
         for(int i =0 ;i < numRows;i++)
         {
-            vector<int> v(i+1,1);
+            vector<int> v(i+1,1);  //i+1 is size //setting all values of vector to 1
             for(int j = 1;j < i;j++)
             {
                 v[j] = ans[i-1][j]+ans[i-1][j-1];
