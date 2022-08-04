674a933a7c5899c9526bedefe9b613dc65b542f2
Sanket kumbhar <ksanket114402@gmail.com>
2022-08-04 22:56:47 +0530

    Time: 31 ms (69.34%), Space: 15.1 MB (47.42%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,23 +11,48 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        while(head!=nullptr && head->val == val)
-        {
-            head = head->next;
-        }
+//         while(head!=nullptr && head->val == val)
+//         {
+//             head = head->next;
+//         }
         
-        ListNode *current_node = head;
-        while(current_node!=nullptr && current_node->next!=nullptr)
+//         ListNode *current_node = head;
+//         while(current_node!=nullptr && current_node->next!=nullptr)
+//         {
+//             if(current_node->next->val == val)
+//             {
+//                 current_node->next = current_node->next->next;
+//             }
+//             else
+//             {
+//                 current_node = current_node->next;
+//             }
+//         }
+//         return head;
+        ListNode *cur = head;
+        ListNode *prev = new ListNode(-1);
+        prev->next = head;
+        ListNode *res = prev;
+        while(cur)
         {
-            if(current_node->next->val == val)
+            if(cur->val!=val)
             {
-                current_node->next = current_node->next->next;
+                prev = cur;
+                cur = cur->next;
             }
             else
             {
-                current_node = current_node->next;
+                if(cur->next==nullptr)
+                {
+                    prev->next = nullptr;
+                }  
+                else
+                {
+                    prev->next = cur->next;
+                }
+                cur = cur->next;
             }
         }
-        return head;
+        return res->next;
     }
 };
