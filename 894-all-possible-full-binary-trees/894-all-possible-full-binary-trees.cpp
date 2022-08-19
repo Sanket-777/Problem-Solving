1e812e1dffe12fa3790b7a6b753876b4ecd94e7f
Sanket kumbhar <ksanket114402@gmail.com>
2022-08-19 23:18:04 +0530

    Time: 183 ms (35.60%), Space: 35.9 MB (27.30%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
@@ -11,28 +11,54 @@
  */
 class Solution {
 public:
+     unordered_map<int, vector<TreeNode*>> memo; //for memoisation
     vector<TreeNode*> allPossibleFBT(int n) {
-        if(n == 1)
-        {
-            vector<TreeNode*> v;
-            v.push_back(new TreeNode(0));
-            return v;
-        }
+//         if(n == 1)
+//         {
+//             vector<TreeNode*> v;
+//             v.push_back(new TreeNode(0));
+//             return v;
+//         }
         
-        vector<TreeNode*> allleft,allright;
-        vector<TreeNode*> ans;
-        for(int i = 1;i <  n ;i+=2)
-        {
-            allleft  = allPossibleFBT(i);
-            allright = allPossibleFBT(n-i-1);
+//         vector<TreeNode*> allleft,allright;
+//         vector<TreeNode*> ans;
+//         for(int i = 1;i <  n ;i+=2)
+//         {
+//             allleft  = allPossibleFBT(i);
+//             allright = allPossibleFBT(n-i-1);
             
-            for(int j = 0; j < allleft.size();j++)
+//             for(int j = 0; j < allleft.size();j++)
+//             {
+//                 for(int k = 0;k < allright.size();k++ )
+//                 {
+//                     TreeNode *root = new TreeNode(0);
+//                     root->left = allleft[j];
+//                     root->right = allright[k];
+//                     ans.push_back(root);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// USING MEMOISATION
+        if(memo.find(n)!=memo.end())
+            return memo[n];
+        
+        if(n == 1)
+            return memo[n] = {new TreeNode(0)};
+        
+        if(n%2 == 0)
+            return {};
+        
+        
+        vector<TreeNode*> ans;
+        for(int i = 1;i < n;i+=2)
+        {
+            for(auto l:allPossibleFBT(i))
             {
-                for(int k = 0;k < allright.size();k++ )
+                for(auto r:allPossibleFBT(n-i-1))
                 {
-                    TreeNode *root = new TreeNode(0);
-                    root->left = allleft[j];
-                    root->right = allright[k];
+                    TreeNode *root = new TreeNode(0,l,r);
                     ans.push_back(root);
                 }
             }
