6c8f04517a4947a0a76cea40b702b6679d00234b
Sanket kumbhar <ksanket114402@gmail.com>
2022-09-08 23:18:11 +0530

    Time: 26 ms (15.54%), Space: 7.4 MB (48.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,22 +1,38 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//        int freq[26]={0};
+//        int freq2[26]={0};
+
+        
+//         for(int i=0;i<t.size();i++){
+//             freq[int(t[i])-97]++;
+//         }
+//         for(int i=0;i<s.size();i++){
+//             freq2[int(s[i])-97]++;
+//         }
+//         for(int i=0;i<26;i++){
+//             if(freq[i] != freq2[i])
+//             {
+//                 return false;
+//             }
+//         }
+//         return true; 
+//     }
+// };
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       int freq[26]={0};
-       int freq2[26]={0};
-
+        if (s.size()!=t.size()) return false;
         
-        for(int i=0;i<t.size();i++){
-            freq[int(t[i])-97]++;
+		unordered_map <char, int> umap;
+        for (char c:s) umap[c]++;
+        for(char c:t) {
+            umap[c]--;
+            if (umap[c]<0) return false;
         }
-        for(int i=0;i<s.size();i++){
-            freq2[int(s[i])-97]++;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i] != freq2[i])
-            {
-                return false;
-            }
-        }
-        return true; 
+        
+        return true;
+                    
     }
 };
