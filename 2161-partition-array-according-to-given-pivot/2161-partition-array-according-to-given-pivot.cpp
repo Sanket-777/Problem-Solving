c841950d1c9e056efc894e6bed35cef6fa10715c
Sanket kumbhar <ksanket114402@gmail.com>
2022-09-28 19:02:49 +0530

    Time: 506 ms (13.51%), Space: 123.2 MB (97.36%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,24 +1,61 @@
 class Solution {
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> v1;
-        vector<int> v2;
-        for(int i =  0; i < nums.size();i++)
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         vector<int> v1;
+//         vector<int> v2;
+//         for(int i =  0; i < nums.size();i++)
+//         {
+//             if(nums[i] < pivot)
+//             {
+//               v1.push_back(nums[i]);
+//             }
+//             else if(nums[i] > pivot)
+//             {
+//                 v2.push_back(nums[i]);
+//             }
+//         }
+//         int rep =  nums.size() - (v1.size()+v2.size());
+//         while(rep--)
+//             v1.push_back(pivot);
+        
+//         v1.insert(v1.end(),v2.begin(),v2.end());
+//         return v1;
+//     }
+    vector<int> pivotArray(vector<int> &nums, int pivot)
+    {
+        int low = 0;
+        int same = 0;
+        int high;
+        for (auto n : nums)
         {
-            if(nums[i] < pivot)
+            if (n < pivot)
             {
-              v1.push_back(nums[i]);
+                ++low;
             }
-            else if(nums[i] > pivot)
+            else if (n == pivot)
             {
-                v2.push_back(nums[i]);
+                ++same;
             }
         }
-        int rep =  nums.size() - (v1.size()+v2.size());
-        while(rep--)
-            v1.push_back(pivot);
-        
-        v1.insert(v1.end(),v2.begin(),v2.end());
-        return v1;
+        vector<int> res(nums.size());
+        high = same + low;
+        same = low;
+        low = 0;
+        for (auto n : nums)
+        {
+            if (n < pivot)
+            {
+                res[low++] = n;
+            }
+            else if (n == pivot)
+            {
+                res[same++] = n;
+            }
+            else
+            {
+                res[high++] = n;
+            }
+        }
+        return res;
     }
 };
