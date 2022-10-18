2b77f7d9acc5bc07cf1434ef2ac73509239fd089
Sanket kumbhar <ksanket114402@gmail.com>
2022-10-18 23:51:49 +0530

    Time: 3 ms (92.28%), Space: 12 MB (74.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,20 +4,20 @@ public:
         int n  = p.size(),k,l;
   for(k = n-2;k>=0;k--)
   {
-      if(p[k]<p[k+1])
+      if(p[k]<p[k+1]) //arr[i] < arr[i+1]
           break;
   }
     
     if(k < 0)
     {
-        reverse(p.begin(),p.end());
+        reverse(p.begin(),p.end()); //best case is not inflation point
     }
     else
     {
-        for(l = n-1;l > k;l--)
+        for(l = n-1;l > 0;l--)
         {
             if(p[l]>p[k])
-            {
+            {                               
                 break;
             }
         }
