1fb752891c4e2c42e7379fc4029b9a847969c655
Sanket kumbhar <ksanket114402@gmail.com>
2022-10-25 23:05:14 +0530

    Time: 425 ms (20.87%), Space: 34.7 MB (31.10%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int numTriplets(vector<int>& nums1, vector<int>& nums2) {
-       unordered_map <long,long> mp;
-         unordered_map <long,long> mp2;
-        for(long i:nums1)
+    int cnt = 0;
+    int calc(vector<int>& nums1, vector<int>& nums2)
+    {
+        unordered_map<long,long> mp;
+         for(int i:nums1)
         {
-            mp[i*i]++;
+            mp[(long)i*i]++;
         }
-        int cnt= 0;
         sort(nums2.begin(),nums2.end());
         for(int i = 0;i < nums2.size();i++)
         {
@@ -19,20 +19,12 @@ public:
                 }
             }
         }
-        for(long i:nums2)
-        {
-            mp2[i*i]++;
-        }
-        for(int i = 0;i < nums1.size();i++)
-        {
-            for(int j = i+1;j < nums1.size();j++)
-            {
-                if(mp2[(long)nums1[i]*nums1[j]]>0)    
-                {
-                    cnt+=mp2[(long)nums1[i]*nums1[j]];
-                }
-            }
-        }
+        return cnt;
+    }
+    int numTriplets(vector<int>& nums1, vector<int>& nums2) {
+       
+        calc(nums1,nums2);
+        calc(nums2,nums1);
         return cnt;
     }
 };
