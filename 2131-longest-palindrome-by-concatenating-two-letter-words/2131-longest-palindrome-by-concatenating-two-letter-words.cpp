f25dbfb5738f5e898917cc501f85ef57dd8fe2ed
Sanket kumbhar <ksanket114402@gmail.com>
2022-11-03 22:06:54 +0530

    Time: 746 ms (57.07%), Space: 168 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -4,25 +4,29 @@ public:
     unordered_map<string, int> m;
     int unpaired = 0, ans = 0;
     for (string w: words) {
-        if (w[0] == w[1]) {
-            if (m[w] > 0) {
-                unpaired--;
-                m[w]--;
-                ans += 4;
+        if (w[0] == w[1]) {     //checking if both the letters are same or not
+            if (m[w] > 0) {    // checking if the  we found multipe count of same words
+                unpaired--;     // we found a matching word so --
+                m[w]--;        //removing that word from the map
+                ans += 4;       //when we get two letters the cnt becomes 4 so we add 4 to them
             }
             else {
-                m[w]++;
-                unpaired++;
+                m[w]++;     
+                                //if it is not present in map add it in map
+                unpaired++;     //mark it as unpaired
             }
         }
         else {
-            string rev = w;
+            string rev = w;   //letters are not equal
             reverse(rev.begin(), rev.end());
             if (m[rev] > 0) {
                 ans += 4;
                 m[rev]--;
             }
-            else m[w]++;
+            else 
+            {
+                    m[w]++;
+            }
         }
     }
     if (unpaired > 0) ans += 2;
