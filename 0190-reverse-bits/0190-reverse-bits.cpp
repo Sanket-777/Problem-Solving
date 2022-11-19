aa55b1a614720a31ff105ba36d3414a6a3f0b862
Sanket kumbhar <ksanket114402@gmail.com>
2022-11-19 13:48:00 +0530

    Time: 2 ms (79.62%), Space: 6 MB (50.97%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool findkthbit(int k,uint32_t n)
+    {
+        if(((1<<(k-1))&n)!=0) return true;
+        return false;
+    }
+
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for(int i =1;i <= 32;i++)
+        {
+            if(findkthbit(i,n))
+            {
+                ans  = 1 << (32-i) | ans;
+            }
+        }
+        return ans;
+    }
+};
